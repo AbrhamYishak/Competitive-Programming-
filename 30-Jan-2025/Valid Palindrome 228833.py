69b6c9aa28c9774a1af438b1ec53beb2694b520b
AbrhamYishak <136889996+philapheda@users.noreply.github.com>
2025-01-30 12:11:28 -0800

    Created 'Valid Palindrome' by A2SV Hub

---
+++ 30-Jan-2025/Valid Palindrome 228833.py
@@ -0,0 +1,16 @@
+# Problem: Valid Palindrome - https://leetcode.com/problems/valid-palindrome/description/
+
+class Solution(object):
+    def isPalindrome(self, s):
+        a = s.split(" ")
+        ans = ""
+        for i in a:
+            for j in i:
+                if j.isalnum():
+                    ans+=j
+        return ans.lower() == ans[::-1].lower()
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
