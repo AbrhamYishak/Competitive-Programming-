9a35b2125fdae7c0f669c36617513dfd049a5dc2
AbrhamYishak <136889996+philapheda@users.noreply.github.com>
2025-01-30 04:24:43 -0800

    Created 'Convert the Temperature' by A2SV Hub

---
+++ 30-Jan-2025/Convert the Temperature 226613.py
@@ -0,0 +1,10 @@
+# Problem: Convert the Temperature - https://leetcode.com/problems/convert-the-temperature/
+
+class Solution(object):
+    def convertTemperature(self, celsius):
+        return [celsius + 273.15,celsius * 1.80 + 32.00]
+        """
+        :type celsius: float
+        :rtype: List[float]
+        """
+        
