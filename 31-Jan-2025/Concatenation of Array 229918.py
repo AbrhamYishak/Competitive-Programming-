416a73c0004aeb062157a62652444a6f6b65eb07
AbrhamYishak <136889996+philapheda@users.noreply.github.com>
2025-01-30 23:39:19 -0800

    Created 'Concatenation of Array' by A2SV Hub

---
+++ 31-Jan-2025/Concatenation of Array 229918.py
@@ -0,0 +1,10 @@
+# Problem: Concatenation of Array - https://leetcode.com/problems/concatenation-of-array/description/
+
+class Solution(object):
+    def getConcatenation(self, nums):
+        return nums+nums
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
