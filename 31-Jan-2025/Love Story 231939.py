11b39a1293e1b2b987634daefbfadb3bc74018d2
AbrhamYishak <136889996+philapheda@users.noreply.github.com>
2025-01-31 10:01:28 -0800

    Created 'Love Story' by A2SV Hub

---
+++ 31-Jan-2025/Love Story 231939.py
@@ -0,0 +1,11 @@
+# Problem: Love Story - https://codeforces.com/contest/1829/problem/A
+
+t = int(input())
+for _ in range(t):
+    count = 0
+    s = input()
+    word = "codeforces"
+    for i in range(10):
+        if s[i]!=word[i]:
+            count+=1
+    print(count)
