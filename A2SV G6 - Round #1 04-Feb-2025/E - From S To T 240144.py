57e1c87d8106414adf56ec47a02569d599fbcb44
AbrhamYishak <136889996+philapheda@users.noreply.github.com>
2025-02-04 05:23:58 -0800

    Created 'E - From S To T' by A2SV Hub

---
+++ A2SV G6 - Round #1 04-Feb-2025/E - From S To T 240144.py
@@ -0,0 +1,30 @@
+# Problem: E - From S To T - https://codeforces.com/gym/585107/problem/E
+
+from collections import *
+q = int(input())
+for _ in range(q):
+    s = input()
+    t = input()
+    p = Counter(input())
+    j = 0
+    done = False
+    for i in range(len(t)):
+        if j < len(s) and s[j] == t[i]:
+            j+=1
+        else:
+            if t[i] in p and p[t[i]] > 0:
+                p[t[i]]-=1
+            else:
+                print("NO")
+                done = True
+                break
+    if not done and j != len(s):
+        print("NO")
+        done = True
+    if not done:
+        print("YES")
+
+
+    
+    
+
