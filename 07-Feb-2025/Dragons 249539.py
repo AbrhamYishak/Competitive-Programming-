e801bd8b576bba50d4ea65df07030be7dcc52a2a
AbrhamYishak <136889996+philapheda@users.noreply.github.com>
2025-02-07 03:24:59 -0800

    Created 'Dragons' by A2SV Hub

---
+++ 07-Feb-2025/Dragons 249539.py
@@ -0,0 +1,25 @@
+# Problem: Dragons - https://codeforces.com/problemset/problem/230/A
+
+###########--------Template-----------#########
+from math import *
+from collections import *
+def num(): return (int(input()))
+def string(): return (input())
+def arr(): return list(map(int, input().split()))
+def arrstr(): return list(map(str, input().split()))
+###########--------Template-----------#########
+s,n = arr()
+dragons = []
+for _ in range(n):
+    dragons.append(arr())
+dragons.sort(key=lambda x : x[0])
+done = False
+for i in dragons:
+    if s <= i[0]:
+        print("NO")
+        done = True
+        break
+    else:
+        s+=i[1]
+if not done:
+    print("YES")
