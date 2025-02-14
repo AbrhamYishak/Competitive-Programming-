184192d4f683aafd417590e7b4263e7b3ed6a4ce
AbrhamYishak <136889996+philapheda@users.noreply.github.com>
2025-02-14 16:47:25 +0300

    Created 'Books' by A2SV Hub

---
+++ 14-Feb-2025/Books 265990.py
@@ -0,0 +1,21 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+###########--------Template-----------#########
+from math import *
+from collections import *
+def num(): return (int(input()))
+def string(): return (input())
+def arr(): return list(map(int, input().split()))
+def arrstr(): return list(map(str, input().split()))
+###########--------Template-----------#########
+n,t = arr()
+book = arr()
+start = 0
+ans = 0
+for end in range(n):
+    t-=book[end]
+    while t < 0:
+        t+=book[start]
+        start+=1
+    ans=max(ans,end-start+1)
+print(ans)
