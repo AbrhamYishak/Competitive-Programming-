657d746c4c08e94d7f6e30a6b1b00582ae5a7bf3
AbrhamYishak <136889996+philapheda@users.noreply.github.com>
2025-02-16 23:10:56 +0300

    Created 'C - The Splitting Game' by A2SV Hub

---
+++ A2SV G6 - Round #2 16-Feb-2025/C - The Splitting Game 267947.py
@@ -0,0 +1,25 @@
+# Problem: C - The Splitting Game - https://codeforces.com/gym/586960/problem/C
+
+###########--------Template-----------#########
+from math import *
+from collections import *
+def num(): return (int(input()))
+def string(): return (input())
+def arr(): return list(map(int, input().split()))
+def arrstr(): return list(map(str, input().split()))
+###########--------Template-----------#########
+t = num()
+for _ in range(t):
+    n = num()
+    s = string()
+    half = Counter(s)
+    other_half = set()
+    ans = 0
+    for i in s:
+        other_half.add(i)
+        half[i]-=1
+        if not half[i]:
+            del half[i]
+        ans = max(ans,len(half)+len(other_half))
+    print(ans)
+
