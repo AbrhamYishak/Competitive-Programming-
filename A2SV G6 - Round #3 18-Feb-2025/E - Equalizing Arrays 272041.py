6c589d45e4b789dfe18007e7c1684c1dc0571d08
AbrhamYishak <136889996+philapheda@users.noreply.github.com>
2025-02-18 17:13:08 +0300

    Created 'E - Equalizing Arrays' by A2SV Hub

---
+++ A2SV G6 - Round #3 18-Feb-2025/E - Equalizing Arrays 272041.py
@@ -0,0 +1,41 @@
+# Problem: E - Equalizing Arrays - https://codeforces.com/gym/588468/problem/E
+
+###########--------Template-----------#########
+from math import *
+from collections import *
+def num(): return (int(input()))
+def string(): return (input())
+def arr(): return list(map(int, input().split()))
+def arrstr(): return list(map(str, input().split()))
+###########--------Template-----------#########
+n = num()
+a = arr()
+m = num()
+b = arr()
+end1 = 0
+end2 = 0
+sum1 = a[0]
+sum2 = b[0]
+count = 0
+while end1 < n and end2 < m:
+    while sum1 != sum2 and end1 < n and end2 < m:
+        if sum1 < sum2:
+            end1+=1
+            if end1 < n:
+                sum1+=a[end1]
+        elif sum2 < sum1:
+            end2+=1
+            if end2 < m:
+                sum2+=b[end2]
+    if sum1 == sum2:
+        count+=1
+        end1+=1
+        end2+=1
+        if end1 < n and end2 < m:
+            sum1=a[end1]
+            sum2=b[end2]
+if count < 1 or end1 != n or end2 != m:
+    print(-1)
+else:
+    print(count)
+    
