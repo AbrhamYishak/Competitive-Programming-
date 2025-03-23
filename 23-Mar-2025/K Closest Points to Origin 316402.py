9d1ce14f51d5a33c356a733df2181985211851da
AbrhamYishak <136889996+philapheda@users.noreply.github.com>
2025-03-23 23:49:45 +0300

    Created 'K Closest Points to Origin' by A2SV Hub

---
+++ 23-Mar-2025/K Closest Points to Origin 316402.py
@@ -0,0 +1,21 @@
+# Problem: K Closest Points to Origin - https://leetcode.com/problems/k-closest-points-to-origin/
+
+from collections import defaultdict
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        distance = defaultdict(list)
+        ans = []
+        for i in points:
+            x,y = i[0],i[1]
+            dis = (x**2+y**2)**(1/2)
+            if dis not in distance:
+                distance[dis] = [i]
+            else:
+                distance[dis].append(i)
+        sorted_dis = dict(sorted(distance.items()))
+        for i in sorted_dis:
+            for j in sorted_dis[i]:
+                if len(ans)!=k:
+                    ans.append(j)
+        return ans
+        
